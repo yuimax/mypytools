72f323cc00d68eb3e5d55f90255f91929780e6b0
yuimax <yuimaroid@yahoo.co.jp>
2025-09-24 00:52:12 +0900

    commit 2025/09/24 00:52:12 (UST+9)

--- test/myftp-test.py
+++ test/myftp-test.py
@@ -3,11 +3,11 @@ import myftp
 myftp.verbose(False)
 target_dir = './sample'
 
-for name in myftp.get_ftp_names():
+for ftp_name in myftp.get_ftp_names():
     try:
-        myftp.remove_tree(name, target_dir)
-        myftp.upload_tree(name, target_dir)
-        myftp.ls(name, target_dir)
+        myftp.remove_tree(ftp_name, target_dir)
+        myftp.upload_tree(ftp_name, target_dir)
+        myftp.ls(ftp_name, target_dir)
         print()
     except Exception as e:
         print(f"ERROR: {e}")
